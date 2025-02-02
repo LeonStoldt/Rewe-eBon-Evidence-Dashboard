3ad99edfc90a60563d1219437beb21ec7eaefb67
Leon Stoldt <tech@leon-stoldt.de>
2025-02-02 17:47:19 +0100

    add coordinates to marketdata

--- sources/rewe_ebon_extractor/rewe_ebon_extractor.js
+++ sources/rewe_ebon_extractor/rewe_ebon_extractor.js
@@ -1,8 +1,23 @@
 import path from 'path';
 import { readFileSync, readdirSync } from 'fs';
+import fetch from 'node-fetch';
 import { parseEBon } from 'rewe-ebon-parser';
 
 const pdfFolder = path.join(process.cwd(), 'static', 'ebons');
+
+async function getLatLong(city, zip, street) {
+    const query = `${street}, ${zip}, ${city}, Germany`;
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.length > 0) {
+        return { lat: data[0].lat, lon: data[0].lon };
+    }
+    console.error(`could not find geolocation of zip=${zip}, city=${city} and street=${street}`)
+    return { lat: null, lon: null };
+}
+
 
 async function parseEbons() {
   const files = readdirSync(pdfFolder).filter((file) => file.endsWith('.pdf'));
@@ -16,6 +31,12 @@ async function parseEbons() {
 //        console.log(`Parsed data for ${file}:`, JSON.stringify(receipt, null, 2));
 
         if (receipt.marketAddress) {
+          let marketData = receipt.marketAddress
+          if (marketData.city && marketData.zip && marketData.street) {
+            let marketGeolocation = await getLatLong(marketData.city, marketData.zip, marketData.street)
+            receipt.marketAddress.lat = marketGeolocation.lat
+            receipt.marketAddress.lon = marketGeolocation.lon
+          }
           receipt.marketAddress = JSON.stringify(receipt.marketAddress);
         }
         if (receipt.items) {
